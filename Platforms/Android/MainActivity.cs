bcede5940469030cb1537cc04a5f66df77f2d989
Long Doan Van <doanlong266@gmail.com>
2024-03-25 15:10:53 +0700

    configure file

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -8,12 +8,5 @@ namespace Crypto_NFT_UI
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
-            Window.Attributes.LayoutInDisplayCutoutMode = LayoutInDisplayCutoutMode.ShortEdges;
-        }
     }
 }
